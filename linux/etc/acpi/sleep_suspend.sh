04718521ed3a850f35bf83b18c208b1faf93372e
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2012-04-25 05:02:45 +0400

    acpi hackery

---
+++ linux/etc/acpi/sleep_suspend.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# This script HANDLES the sleep or suspend button (does not TRANSLATE it). It
+# is part of the *suspend* side of acpi-support, not the special keys
+# translation side. If this script is called, it is assumed to be the result of
+# a suspend key press that can also be heard by other parts of the system. The
+# only time that it actually does something is when it is determined that no
+# other parts of the system are listening (this is what the CheckPolicy call
+# does).
+
+test -f /usr/share/acpi-support/key-constants || exit 0
+
+. /etc/default/acpi-support
+. /usr/share/acpi-support/policy-funcs
+
+if CheckPolicy; then
+    exit;
+fi
+
+if [ x$1 = xsleep ] && [ x$ACPI_SLEEP != xtrue ]; then
+  exit;
+fi
+
+if [ x$1 = xsuspend ] && [ x$ACPI_SLEEP != xtrue ]; then
+  exit;
+fi
+
+if [ x$LOCK_SCREEN = xtrue ]; then
+	. /usr/share/acpi-support/screenblank
+fi
+
+if [ x$1 = xsleep ]; then
+	pm-suspend
+elif [ x$1 = xsuspend ]; then
+	pm-suspend
+fi
+
